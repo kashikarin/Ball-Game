ed4862f4d4b1daa735b49c7a38e44844ea228840
kashikarin <karinkashi@gmail.com>
2025-05-08 22:17:56 +0300

    updating onballclick func to increment ball size

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 function onBallClick(){
-    console.log('ball is clicked!')
+    const elBall = document.querySelector('.ball');
+    const computedStyles = getComputedStyle(elBall);
+    let width = computedStyles.width.replace("px", "");
+    let height = computedStyles.height.replace("px", "");
+    console.log(`${+width + 50}px`)
+    elBall.style.width = `${+width + 50}px`;
+    elBall.style.height = `${+height + 50}px`;
+    elBall.innerText = `${+width + 50}px`;
 }
